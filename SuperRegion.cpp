9bc1baca60ac85d5dcaf9462bddde9c2a5dd6823
bote795 <nbotell2@gmail.com>
2015-04-28 03:34:56 -0500

    added methods to SuperRegion
    added way to calculate how many wastelands and how many neutral enemeies
    there is on starting superRegions

--- SuperRegion.cpp
+++ SuperRegion.cpp
@@ -2,7 +2,7 @@
 #include "SuperRegion.h"
 
 SuperRegion::SuperRegion()
-	: reward(0)
+	: reward(0), Counterwastelands(0), neutrals(0), WASTESIZE(6)
 {
 }
 
@@ -18,4 +18,22 @@ SuperRegion::~SuperRegion()
 void SuperRegion::addRegion(const int& region)
 {
 	regions.push_back(region);
+}
+void SuperRegion::calcWastelands(std::vector<int> Wastelands)
+{
+  for(int j=0; j < Wastelands.size(); ++j)
+	for (int i = 0; i < regions.size(); ++i)
+	{
+		if (regions[i] == Wastelands[j])
+		{
+			Counterwastelands = Wastelands;
+			break;
+		}
+	}
+  calcNeutrals();	
+}
+void SuperRegion::calcNeutrals()
+{
+	neutrals = regions.size()*2;
+	neutrals += Counterwastelands* WASTESIZE;
 }
